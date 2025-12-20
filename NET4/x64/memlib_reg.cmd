ccecdd4079cca003ee74a7bcddbd28fe10ee7970
Arkady <kornienko@outlook.com>
2025-12-20 16:56:40 +1000

    Add files via upload

---
+++ NET4/x64/memlib_reg.cmd
@@ -0,0 +1,5 @@
+REm =======================================
+Rem  РЕГИСТРАЦИЯ бтблиотеки memlib.net.dll
+Rem =======================================
+
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\regasm.exe memlib.net.dll /codebase /tlb
---
+++ NET4/x64/memlib_unreg.cmd
@@ -0,0 +1,5 @@
+REm ================================================
+Rem  “„€‹…ˆ… …ƒˆ‘’€–ˆˆ ¡â¡«¨®â¥ª¨ memlib.net.dll
+Rem ================================================
+
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\regasm.exe memlib.net.dll /unregister
