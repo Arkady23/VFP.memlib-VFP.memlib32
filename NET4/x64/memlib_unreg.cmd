3f421d384d1de0ceeb7b806a2ccbef0b7421f160
Arkady <kornienko@outlook.com>
2025-12-20 17:11:12 +1000

    Add files via upload

--- NET4/x64/memlib_unreg.cmd
+++ NET4/x64/memlib_unreg.cmd
@@ -1,5 +1,5 @@
-REm ================================================
-Rem  “„€‹…ˆ… …ƒˆ‘’€–ˆˆ ¡â¡«¨®â¥ª¨ memlib.net.dll
-Rem ================================================
+Rem =========================================================
+Rem  REMOVING the registration of the memlib.net.dll library
+Rem =========================================================
 
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\regasm.exe memlib.net.dll /unregister
