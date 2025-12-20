32b4c93a40c5400936141b71d17079fed61c7c87
Arkady <kornienko@outlook.com>
2025-12-20 17:21:28 +1000

    Update memlib_unreg.cmd

--- NET4/x64/memlib_unreg.cmd
+++ NET4/x64/memlib_unreg.cmd
@@ -1,5 +1,5 @@
-Rem =========================================================
-Rem  REMOVING the registration of the memlib.net.dll library
-Rem =========================================================
+Rem ==========================================
+Rem  UNREGISTRATION of library memlib.net.dll
+Rem ==========================================
 
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\regasm.exe memlib.net.dll /unregister
