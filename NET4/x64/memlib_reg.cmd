4ae068e6d91de9a897316d1ab9d0271a1cb9ccfe
Arkady <kornienko@outlook.com>
2026-05-27 08:34:12 +1000

    Update memlib_reg.cmd

--- NET4/x64/memlib_reg.cmd
+++ NET4/x64/memlib_reg.cmd
@@ -3,3 +3,4 @@ Rem  REGISTRATION of library memlib.net.dll
 Rem ========================================
 
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\regasm.exe memlib.net.dll /codebase /tlb
+pause
